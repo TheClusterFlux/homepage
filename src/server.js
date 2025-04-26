33487fe5b54b38943c7de195c1188eeb43a1a1cc
Keanu Watts <keanu.watts1@gmail.com>
2025-04-26 04:47:17 +0200

    fixed pathing

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const multer = require('multer');
-const { fetchAndSaveHomepageData,addProjectWithImage,addCreator } = require('./datamanagement');
+const { fetchAndSaveHomepageData,addProjectWithImage,addCreator } = require('./src/datamanagement');
 const fs = require('fs');
 
 const app = express();
